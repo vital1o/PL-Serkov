60a1ca18fac03020130c6b526cf86fefc6fd03ee
vital1o <vitaliyserkov2000@gmail.com>
2024-10-11 19:51:52 +0300

    Delete 4_2.py

--- 4_2.py
+++
@@ -1,9 +0,0 @@
-A = int(input("Введите A: "))
-B = int(input("Введите B: "))
-
-if A < B:
-    for num in range(A, B + 1):
-        print(num)
-else:
-    for num in range(A, B - 1, -1):
-        print(num)
