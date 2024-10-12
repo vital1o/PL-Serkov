92ef29632c86096668f97c66e176c5f7ab143a57
vital1o <vitaliyserkov2000@gmail.com>
2024-10-12 17:11:12 +0300

    Update 5_1.py

--- python/5_1.py
+++ python/5_1.py
@@ -1,2 +1,7 @@
-S=print(input('Введите строку: '))
-# эмммм
+def count_words_starting_with_e():
+    text = input("Введите текст: ")
+    words = text.split()  # разбив на слова
+    count = sum(1 for word in words if word.lower().startswith('е'))  # получение кол-во слов на е
+    return count
+
+print(count_words_starting_with_e())
