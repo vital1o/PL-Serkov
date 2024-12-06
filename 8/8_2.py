fb226dc839118996bcf9490c6bb43a0bec8e6c8d
vital1o <vitaliyserkov2000@gmail.com>
2024-12-06 17:28:33 +0300

    Update 8_2.py

--- 8/8_2.py
+++ 8/8_2.py
@@ -1,19 +1,27 @@
-import numpy as np
+import random
 
 N = int(input("Введите значение N: "))
-M = int(input("Введите значение M:")) 
-B = np.random.randint(-10, 10, size=(N, M)) 
+M = int(input("Введите значение M: "))
+
+# Генерация матрицы B
+B = [[random.randint(-10, 10) for _ in range(M)] for _ in range(N)]
 
 print("Исходная матрица B:")
-print(B)
+for row in B:
+    print(row)
 
 for i in range(N):
-    max_index = np.argmax(B[i]) 
-    min_index = np.argmin(B[i]) 
+    max_index = 0
+    min_index = 0
+    for j in range(1, M):
+        if B[i][j] > B[i][max_index]:
+            max_index = j
+        if B[i][j] < B[i][min_index]:
+            min_index = j
     
     B[i][0], B[i][max_index] = B[i][max_index], B[i][0]
-    
     B[i][-1], B[i][min_index] = B[i][min_index], B[i][-1]
 
 print("Матрица B после замены:")
-print(B)
+for row in B:
+    print(row)
