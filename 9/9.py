22f4809878eedc20ffa204369f16daf394a131b5
vital1o <vitaliyserkov2000@gmail.com>
2024-12-06 17:35:45 +0300

    Update 9.py

--- 9/9.py
+++ 9/9.py
@@ -1,11 +1,10 @@
+# block a
 from math import *
-
 print('№1')
-
 def f(x,n):
     return (x**n)/factorial(n)
-a = int(input())
-b = int(input())
+a = int(input("Введите значение: "))
+b = int(input("Введите значение: "))
 print(f(a,b))
 
 print('-'*6)
@@ -15,20 +14,21 @@ def f(n):
     a = n[::-1]
     return a
 print(f(input()))
-#block B
 
-def dev(n, divisor):
+#block b
+
+def prosto(n, divisor):
   if n <= 1:
     return False
   if divisor * divisor > n:
     return True
   if n % divisor == 0:
     return False
-  return dev(n, divisor + 1)
+  return prosto(n, divisor + 1)
 
 n = int(input("Введите натуральное число n>1: "))
 
-if dev(n, 2):
+if prosto(n, 2):
   print("Да")
 else:
   print("Нет")
