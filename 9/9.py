08b5903f1a04dfe0a53c6f3470e4d92466f31d45
vital1o <vitaliyserkov2000@gmail.com>
2024-12-06 17:37:20 +0300

    Update 9.py

--- 9/9.py
+++ 9/9.py
@@ -14,21 +14,21 @@ def f(n):
     a = n[::-1]
     return a
 print(f(input()))
-
+                      
 #block b
 
-def prosto(n, divisor):
+def div(n, divisor):
   if n <= 1:
     return False
   if divisor * divisor > n:
     return True
   if n % divisor == 0:
     return False
-  return prosto(n, divisor + 1)
+  return div(n, divisor + 1)
 
 n = int(input("Введите натуральное число n>1: "))
 
-if prosto(n, 2):
+if div(n, 2):
   print("Да")
 else:
   print("Нет")
